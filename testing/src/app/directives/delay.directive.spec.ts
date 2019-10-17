12a36587c56ed635b23c1ec43e93c1c11cb41ef1
Eugenio Garcia San Martin <egsmartin@gmail.com>
2019-10-17 23:04:02 +0200

    Ejecucion asincrona

--- testing/src/app/directives/delay.directive.spec.ts
+++ testing/src/app/directives/delay.directive.spec.ts
@@ -28,16 +28,23 @@ describe('DelayDirective', () => {
     el = fixture.debugElement.nativeElement;
   });
 
+  //Probamos usando fakeAsync. Nuestro test se ejecuta de forma sincrona. Dentro de este handler la funcion tick esta disponible
   it('should show after the specified delay using fakeAsync', fakeAsync(() => {
+    //Comprueba el contenido en la vista
     expect(el.innerHTML).not.toContain('DELAYED');
+    //Dispara el control de cambios
     fixture.detectChanges();
+    //Espera 10 ms
     tick(10);
+    //Verifica la vista
     expect(el.innerHTML).toContain('DELAYED');
   }));
 
+  //En este caso vamos a hacer el test de forma asincrona
   it('should show after the specified delay using async', async(() => {
     expect(el.innerHTML).not.toContain('DELAYED');
     fixture.detectChanges();
+    //Esta es la logica asincrona. Esperamos a que nuestro control haya terminado de procesar los cambios
     fixture.whenStable().then(() => {
       fixture.detectChanges();
       expect(el.innerHTML).toContain('DELAYED');
